6343cd69dec63a70b27fe115683ebcf1c6449dc6
Lasyanga <herminigildojrquiano@gmail.com>
2021-07-04 23:56:40 +0800

    first commit

---
+++ custom/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        dots: true,
+        margin: 5,
+        responsive:{
+            0:{
+                items: 1,
+                dots: true
+            },
+            300:{
+                items: 1,
+                dots: true,
+                loop: false
+            },
+            500:{
+                items: 2,
+                dots: true,
+                loop: false
+            },
+            600:{
+                items:3,
+                dots: true,
+                loop: false
+            },
+            1000:{
+                items:5,
+                dots:true,
+                loop:false,
+            }
+        }
+    });
+
+
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+             name: '.name',
+             price: '.price parseInt'
+        }
+    });
+
+    $('#sort').on("click", "button", function() {
+        var sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({ sortBy: sortByValue });
+    });
+
+});
+
+
+// for admin side navbar
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
